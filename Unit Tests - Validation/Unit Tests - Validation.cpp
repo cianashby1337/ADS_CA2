3480bdd8bf0997da325e6e0420c3e3816c0cf678
Cian Ashby <57039577+cianashby1337@users.noreply.github.com>
2023-11-19 15:06:22 +0000

    Test method for one root element created

--- Unit Tests - Validation/Unit Tests - Validation.cpp
+++ Unit Tests - Validation/Unit Tests - Validation.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../CA2/Tree.h"
+#include "../CA2/TreeIterator.h"
+#include "../CA2/Validation.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +12,14 @@ namespace UnitTestsValidation
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TestOneRoot)
 		{
+			Tree<std::string> testTree = Tree<std::string>("");
+			TreeIterator<std::string> testTreeIter = TreeIterator<std::string>(&testTree);
+			std::string xml = "<dir><name>ADS_Single_LinkedList_Exercises</name><dir>";
+			Assert::IsTrue(validateXML(xml, testTreeIter));
+			Assert::IsTrue(testTreeIter.node->data == "<dir>");
+			Assert::IsTrue(testTreeIter.node->children->head->data->data == "<dir>");
 		}
 	};
 }
